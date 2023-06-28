9f2f2b848902cef7366656c5102e588ee17d0bc2
leopf <l.pfob@3-klicks.de>
2023-06-28 08:46:53 +0200

    fastapi test

--- tests/test_asgi.py
+++ tests/test_asgi.py
@@ -6,6 +6,7 @@ from streamtasks.worker import *
 from streamtasks.node import *
 import asyncio
 from uuid import uuid4
+from fastapi import FastAPI
 import httpx
 
 class TestASGI(unittest.IsolatedAsyncioTestCase):
@@ -57,51 +58,20 @@ class TestASGI(unittest.IsolatedAsyncioTestCase):
     self.assertEqual([], data.events)
     self.assertEqual(True, data.closed)
 
-  async def test_half_app(self):
-    async def demo_app(scope, receive, send):
-      await send({"type": "http.response.start", "status": 200})
-      await send({"type": "http.response.body", "body": b"Hello world!"})
+  async def test_app_fastapi(self):
+    demo_app = FastAPI()
+    demo_app.add_api_route("/", lambda: { "text": "Hello from FastAPI!" })
 
     runner = ASGIAppRunner(self.client2, demo_app, "demo_app", 1337)
     self.tasks.append(asyncio.create_task(runner.async_start(self.stop_signal)))
 
-    async def proxy_app(scope, receive, send):
-      connection_id = str(uuid4())
-      closed_signal = asyncio.Event()
-
-      await self.client1.fetch(1337, "demo_app", ASGIInitMessage(
-        scope=JSONValueTransformer.annotate_value(scope), 
-        connection_id=connection_id, 
-        return_address=1338))
-      
-      async def send_loop():
-        sender = ASGIEventSender(self.client1, 1337, connection_id)
-        while not closed_signal.is_set():
-          event = await receive()
-          await sender.send(event)
-          await asyncio.sleep(0.001)
-
-      async def recv_loop():
-        receiver = ASGIEventReceiver(self.client1, 1338)
-        with receiver:
-          while True:
-            data = await receiver.recv()
-            for event in data.events:
-              await send(MessagePackValueTransformer.deannotate_value(event))
-            if data.closed: break
-        closed_signal.set()
-
-      recv_loop_task = asyncio.create_task(recv_loop())
-      send_loop_task = asyncio.create_task(send_loop())
-      await closed_signal.wait()
-      send_loop_task.cancel()
-      recv_loop_task.cancel()
-
+    proxy_app = ASGIProxyApp(self.client1, 1337, "demo_app", 1338)
+    
     transport = httpx.ASGITransport(app=proxy_app)
     client = httpx.AsyncClient(transport=transport, base_url="http://testserver")
     response = await client.get("/")
     self.assertEqual(200, response.status_code)
-    self.assertEqual(b"Hello world!", response.content)
+    self.assertEqual(b'{"text":"Hello from FastAPI!"}', response.content)
 
   async def test_app(self):
     async def demo_app(scope, receive, send):
