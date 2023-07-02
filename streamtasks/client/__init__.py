3eef03a02555f6099119fdd179c09b7406db6238
leopf <l.pfob@3-klicks.de>
2023-07-02 17:29:21 +0200

    moved some helpers

--- streamtasks/client/__init__.py
+++ streamtasks/client/__init__.py
@@ -6,60 +6,9 @@ from streamtasks.system.protocols import WorkerAddresses, WorkerFetchDescriptors
 from streamtasks.message.serializers import get_core_serializers
 from streamtasks.message.data import *
 from streamtasks.client.receiver import *
-from streamtasks.client.fetch import *
+from streamtasks.client.fetch import FetchReponseReceiver, FetchRequest, FetchRequestMessage, FetchRequestReceiver, FetchResponseMessage, FetchServerReceiver
+from streamtasks.client.helpers import ProvideContext, ProvideTracker, SubscibeContext, SubscribeTracker
 import secrets
-
-class SubscibeContext:
-  def __init__(self, client: 'Client', topics: Iterable[int]):
-    self._client = client
-    self._topics = topics
-  async def __aenter__(self): await self._client.subscribe(self._topics)
-  async def __aexit__(self, *args): await self._client.unsubscribe(self._topics)
-class ProvideContext:
-  def __init__(self, client: 'Client', topics: Iterable[int]):
-    self._client = client
-    self._topics = topics
-  async def __aenter__(self): await self._client.provide(self._topics)
-  async def __aexit__(self, *args): await self._client.unprovide(self._topics)
-
-class SubscribeTracker:
-  def __init__(self, client: 'Client'):
-    self._client = client
-    self._topic = None
-    self._subscribed = False
-  async def subscribe(self): 
-    if not self._subscribed: 
-      self._subscribed = True
-      await self._client.subscribe([self._topic])
-  async def unsubscribe(self): 
-    if self._subscribed: 
-      self._subscribed = False
-      await self._client.unsubscribe([self._topic])
-  @property
-  def topic(self): return self._topic
-  async def set_topic(self, topic: int): 
-    if self._topic is not None: await self._client.unsubscribe([ self._topic ])
-    self._topic = topic
-    if self._topic is not None: await self._client.subscribe([ self._topic ])
-class ProvideTracker:
-  def __init__(self, client: 'Client'):
-    self._client = client
-    self._topic = None
-    self._paused = False
-  async def pause(self):
-    if not self._paused:
-      self._paused = True
-      await self._client.send_stream_control(self._topic, TopicControlData(paused=True))
-  async def resume(self):
-    if self._paused:
-      self._paused = False
-      await self._client.send_stream_control(self._topic, TopicControlData(paused=False))
-  @property
-  def topic(self): return self._topic
-  async def set_topic(self, topic: int):
-    if self._topic is not None: await self._client.unprovide([ self._topic ])
-    self._topic = topic
-    if self._topic is not None: await self._client.provide([ self._topic ])
 
 class Client:
   _connection: Connection
