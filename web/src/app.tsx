b530cb55815bdc9b55364ea3c742cb808decdb96
leopf <l.pfob@3-klicks.de>
2024-03-30 06:40:33 +0100

    editor works

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,8 +1,25 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { NodeEditorRenderer } from './lib/node-editor';
+import { ConnectResult, InputConnection, NodeEditorRenderer, OutputConnection } from './lib/node-editor';
+import { Node } from "./lib/node-editor";
 
 window.React = React;
+
+class DemoNode implements Node {
+    public id: string = String(Math.random());
+    public name: string = "Test";
+    public position: { x: number; y: number; } = { x: 200 + 100 * Math.random(), y: 200 + 100 * Math.random() };
+    public outputs: OutputConnection[] = [Math.floor(Math.random() * 1000000), Math.floor(Math.random() * 1000000)].map((id, idx) => ({ id, streamId: id, label: `out ${idx + 1}` }));
+    public inputs: InputConnection[] = [String(Math.random() * 1000000), String(Math.random() * 1000000)].map((id, idx) => ({ id, key: id, label: `in ${idx + 1}` }));
+
+    public async connect(key: string, output?: OutputConnection | undefined): Promise<ConnectResult> {
+        const input = this.inputs.find(i => i.key == key);
+        if (!input) return "Input not found!";
+        input.streamId = output?.streamId;
+        return true;
+    }
+    onUpdated?: ((cb: () => void) => void) | undefined;
+}
 
 function App() {
     const containerRef = useRef<HTMLDivElement>(null);
@@ -10,7 +27,8 @@ function App() {
 
     useEffect(() => {
         nodeRenderer.mount(containerRef.current!);
-        // nodeRenderer.addNode()
+        nodeRenderer.addNode(new DemoNode())
+        nodeRenderer.addNode(new DemoNode())
     }, [])
 
     return <div style={{ width: "100vw", height: "100vh" }} ref={containerRef}></div>
