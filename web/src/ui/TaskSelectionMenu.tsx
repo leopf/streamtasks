0c094f0734e88af868ab7b83babf397dadbc79f5
leopf <l.pfob@3-klicks.de>
2024-03-31 18:17:59 +0200

    ui

--- web/src/ui/TaskSelectionMenu.tsx
+++ web/src/ui/TaskSelectionMenu.tsx
@@ -1,15 +1,22 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, TextField } from "@mui/material";
 import { TaskTemplateItem } from "./TaskTemplateItem";
-import { observer } from "mobx-react-lite";
+import { observer, useLocalObservable } from "mobx-react-lite";
 import { useGlobalState } from "../state";
 
 export const TaskSelectionMenu = observer(() => {
     const state = useGlobalState();
-    
+    const localState = useLocalObservable(() => ({
+        searchText: "",
+        get foundHosts() {
+            return this.searchText ? state.taskManager.taskHosts.filter(th => Object.values(th.metadata).some(k => String(k).includes(this.searchText))) : state.taskManager.taskHosts
+        }
+    }));
+
     return (
-        <Box sx={{ height: "100%", maxHeight: "100%", overflowY: "auto" }} bgcolor={"#ccc"}>
-            <Stack boxSizing="border-box" spacing={5} padding={1} minHeight="100%">
-                {state.taskManager.taskHosts.map(th => <TaskTemplateItem key={th.id} taskHost={th} />)}
+        <Box sx={{ height: "100%", maxHeight: "100%", overflowY: "auto" }} bgcolor={"#eee"}>
+            <Stack boxSizing="border-box" spacing={2.5} padding={1} minHeight="100%">
+                <TextField onInput={e => localState.searchText = (e.target as HTMLInputElement).value} variant="outlined" label="search"/>
+                {localState.foundHosts.map(th => <TaskTemplateItem key={th.id} taskHost={th} />)}
             </Stack>
         </Box>
     );
--- web/src/ui/TaskTemplateItem.tsx
+++ web/src/ui/TaskTemplateItem.tsx
@@ -7,12 +7,13 @@ import { renderNodeToImage } from "../lib/node-editor";
 import { useGlobalState } from "../state";
 import { TaskNode } from "../lib/task-node";
 import { TaskHostDragData } from "../types/task-host";
+import memoize from "memoizee";
 
 export const TaskTemplateItem = observer((props: { taskHost: TaskHost }) => {
     const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
     const imageRef = useRef<HTMLImageElement>(null);
     const state = useGlobalState()
-    
+
     const label = useMemo(() => taskHostLabelFields.map(f => props.taskHost.metadata[f]).find(l => l), [props.taskHost]);
     const description = useMemo(() => taskHostDescriptionFields.map(f => props.taskHost.metadata[f]).find(l => l), [props.taskHost]);
     const nodeName = useMemo(() => props.taskHost.metadata["nodename"], [props.taskHost]);
@@ -23,10 +24,15 @@ export const TaskTemplateItem = observer((props: { taskHost: TaskHost }) => {
             .then(imageUrl => setImageUrl(imageUrl));
     }, []);
 
+
+
     return (
         <Box
             width="100%"
             draggable={true}
+            bgcolor="#fff"
+            boxShadow="0 0 5px rgba(0,0,0,0.2)"
+            border="1px solid #cfcfcf"
             onDragStart={(e) => {
                 if (!imageRef.current) return;
                 const imageRect = imageRef.current.getBoundingClientRect()
@@ -34,16 +40,24 @@ export const TaskTemplateItem = observer((props: { taskHost: TaskHost }) => {
                 e.dataTransfer.setData("task_host", JSON.stringify(dragData));
                 e.dataTransfer.setDragImage(imageRef.current, dragData.ox, dragData.oy)
             }}>
-            {imageUrl ? <img ref={imageRef} style={{ width: "100%", display: "block" }} src={imageUrl}/> : (
+            <Box borderBottom="1px solid #cfcfcf" paddingX={1} paddingY={0.5}>
+                <Typography fontWeight="bold" fontSize="0.7rem">node name: {nodeName}</Typography>
+            </Box>
+            {imageUrl ? (
+                <Box padding={3}>
+                    <img ref={imageRef} style={{ width: "100%", display: "block" }} src={imageUrl} />
+                </Box>
+            ) : (
                 <Stack alignItems="center" spacing={2}>
-                    <CircularProgress/>
+                    <CircularProgress />
                     <Typography>{label}</Typography>
                 </Stack>
             )}
-            <Typography>{label}</Typography>
-            <Typography>{description}</Typography>
-            <Typography>{nodeName}</Typography>
-            
+            <Stack marginTop={1} spacing={1} padding={1}>
+                <Typography variant="subtitle2">description: {description || "This is a task for testing purposes. This is a task for testing purposes."}</Typography>
+
+            </Stack>
+
         </Box>
     );
 });
