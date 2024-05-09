25f337c5dd09726b6441bfd580df66ce12ffac58
leopf <l.pfob@3-klicks.de>
2024-05-10 00:12:41 +0200

    th dedupe and sort

--- web/src/ui/TaskSelectionMenu.tsx
+++ web/src/ui/TaskSelectionMenu.tsx
@@ -2,6 +2,7 @@ import { Box, Stack, TextField } from "@mui/material";
 import { TaskTemplateItem } from "./components/TaskTemplateItem";
 import { observer, useLocalObservable } from "mobx-react-lite";
 import { useRootStore } from "../state/root-store";
+import { TaskHost } from "../types/task";
 
 export const TaskSelectionMenu = observer(() => {
     const rootStore = useRootStore();
@@ -12,6 +13,11 @@ export const TaskSelectionMenu = observer(() => {
             if (this.searchText) {
                 taskHosts = taskHosts.filter(th => Object.values(th.metadata).some(k => String(k).includes(this.searchText)));
             }
+            taskHosts = Array.from((new Map<string, TaskHost>(taskHosts.map(th => [th.id, th]))).values())
+            taskHosts = taskHosts
+                .map(th => [th, String(th.metadata["cfg:label"] ?? th.metadata["label"] ?? "")] as [TaskHost, string])
+                .sort((a, b) => a[1].localeCompare(b[1]))
+                .map(item => item[0])
             return taskHosts;
         }
     }));
