dbaee89099792eeb9d40926f4b7ec123c7872bfb
leopf <l.pfob@3-klicks.de>
2024-05-19 07:48:06 +0200

    slider

---
+++ streamtasks/system/tasks/ui/resources/slider.js
@@ -0,0 +1,12 @@
+import { html } from "lit-html";
+
+export function renderUI(value, config, setValue) {
+    const range = config.max_value - config.min_value
+
+    return html`
+    <div class="flex-column">
+        <label style="display:block; margin-left: 1rem;">${config.label}</label>
+        <md-slider .step=${range / 200} .value=${value.value} .min=${config.min_value} .max=${config.max_value} @change=${e => setValue({ value: Number(e.target.value) })}></md-slider>
+    </div>
+    `;
+}
--- streamtasks/system/tasks/ui/resources/switch.js
+++ streamtasks/system/tasks/ui/resources/switch.js
@@ -3,8 +3,8 @@ import { html } from "lit-html";
 export function renderUI(value, config, setValue) {
     return html`
     <div class="flex-row flex-row--center flex-row--spaced">
-        <md-switch id="switch" ?selected=${value.value} @change=${e => setValue({ value: !!e.target.selected })}></md-switch>
-        <label id="label">${config.label}</label>
+        <md-switch ?selected=${value.value} @change=${e => setValue({ value: !!e.target.selected })}></md-switch>
+        <label>${config.label}</label>
     </div>
     `;
 }
