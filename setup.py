9ed1b545913a62d0ceb51937f96dacdbb2dfd1c9
leopf <l.pfob@3-klicks.de>
2024-06-04 01:41:01 +0200

    todos and ext

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import pybind11
 import glob
 
 ext_modules = [ Extension(
-  re.sub(r"[\/\\\\]", ".", p)[:-4],
+  re.sub(r"[\/\\\\]", ".", p)[:-8],
   sources=[p],
   include_dirs=[ pybind11.get_include()],
   language='c++'
-) for p in glob.glob("streamtasks/**/*.cpp", recursive=True)]
+) for p in glob.glob("streamtasks/**/*_all.cpp", recursive=True)]
 
 setup(ext_modules=ext_modules)
