16bbdd8fc643808b4f0d219634167f0d5988bc89
leopf <l.pfob@3-klicks.de>
2024-06-06 07:22:33 +0200

    switch

---
+++ streamtasks/system/tasks/switch.py
@@ -0,0 +1,80 @@
+import asyncio
+from contextlib import AsyncExitStack
+from typing import Any, TypedDict
+from pydantic import BaseModel, ValidationError
+from streamtasks.client.topic import InTopic, SequentialInTopicSynchronizer
+from streamtasks.message.types import NumberMessage
+from streamtasks.net.messages import TopicControlData
+from streamtasks.system.task import Task, TaskHost
+from streamtasks.client import Client
+from streamtasks.utils import context_task
+
+class IOPair(TypedDict):
+  control: int
+  input: int
+
+class SwitchConfig(BaseModel):
+  pairs: list[IOPair] = []
+  output: int
+
+class SwitchTask(Task):
+  def __init__(self, client: Client, config: SwitchConfig):
+    super().__init__(client)
+    sync = SequentialInTopicSynchronizer()
+    self.out_topic = client.out_topic(config.output)
+    self.pairs = [ (client.sync_in_topic(pair["input"], sync), client.sync_in_topic(pair["control"], sync)) for pair in config.pairs ]
+    self.control_values: dict[int, int | float] = {}
+
+  async def run(self):
+    async with AsyncExitStack() as exit_stack:
+      await exit_stack.enter_async_context(self.out_topic)
+      await exit_stack.enter_async_context(self.out_topic.RegisterContext())
+
+      for idx, (in_topic, control_topic) in enumerate(self.pairs):
+        await exit_stack.enter_async_context(in_topic)
+        await exit_stack.enter_async_context(in_topic.RegisterContext())
+        await exit_stack.enter_async_context(control_topic)
+        await exit_stack.enter_async_context(control_topic.RegisterContext())
+        await exit_stack.enter_async_context(context_task(self._run_input_receiver(in_topic, idx)))
+        await exit_stack.enter_async_context(context_task(self._run_control_receiver(control_topic, idx)))
+
+      self.client.start()
+      await asyncio.Future()
+
+  async def _run_input_receiver(self, topic: InTopic, idx: int):
+    while True:
+      data = await topic.recv_data()
+      if self._get_selected_index() == idx:
+        await self.out_topic.set_paused(topic.is_paused)
+        await self.out_topic.send(data)
+
+  async def _run_control_receiver(self, topic: InTopic, idx: int):
+    while True:
+      data = await topic.recv_data_control()
+      try:
+        if isinstance(data, TopicControlData):
+          if data.paused: self.control_values.pop(idx, None)
+        else:
+          message = NumberMessage.model_validate(data.data)
+          await self.set_value(idx, message.value)
+      except ValidationError: pass
+
+  async def set_value(self, idx: int, value: float | int):
+    self.control_values[idx] = value
+    selected_index = self._get_selected_index()
+    if selected_index != -1:
+      await self.out_topic.set_paused(self.pairs[selected_index][0].is_paused)
+
+  def _get_selected_index(self):
+    if len(self.control_values) == 0: return -1
+    selected_value = max(value for value in self.control_values.values())
+    return next(idx for idx, value in self.control_values.items() if value == selected_value)
+
+class SwitchTaskHost(TaskHost):
+  @property
+  def metadata(self): return {
+    "js:configurator": "std:switch",
+    "cfg:label": "Switch"
+  }
+  async def create_task(self, config: Any, topic_space_id: int | None):
+    return SwitchTask(await self.create_client(topic_space_id), SwitchConfig.model_validate(config))
---
+++ web-packages/std-configurators/src/entries/switch.tsx
@@ -0,0 +1,111 @@
+import { GraphSetter, Task, TaskCLSConfigurator, TaskConfiguratorContext, TaskOutput, compareIOIgnorePaths, createCLSConfigurator, extractObjectPathValues, getObjectDiffPaths, parseMetadataField } from "@streamtasks/core";
+import objectPath from "object-path";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+
+export class SwitchConfigurator extends TaskCLSConfigurator {
+    constructor(context: TaskConfiguratorContext, task?: Task) {
+        super(context, task ?? {
+            id: uuidv4(),
+            task_host_id: context.taskHost.id,
+            label: parseMetadataField(context.taskHost.metadata, "cfg:label", z.string(), true),
+            config: { topics: [] },
+            inputs: [],
+            outputs: [{ topic_id: context.idGenerator(), label: "output" }],
+        });
+        this.fix();
+    }
+
+    public  connect(key: string, output?: TaskOutput) {
+        const [input, inputIndex] = this.getInput(key, true);
+        const dcSetter = this.getGraph();
+        dcSetter.set(`inputs.${inputIndex}.topic_id`, undefined);
+        dcSetter.apply();
+
+        if (output) {
+            const setter = this.getGraph();
+            const diffs = getObjectDiffPaths(input, output, compareIOIgnorePaths);
+            setter.set(`inputs.${inputIndex}.topic_id`, output.topic_id);
+            for (const diff of diffs) {
+                setter.set(`inputs.${inputIndex}.${diff}`, objectPath.get(output, diff));
+            }
+            try {
+                setter.apply();
+            }
+            catch {
+                if (input.topic_id === output.topic_id) {
+                    setter.reset();
+                    setter.set(`inputs.${inputIndex}.topic_id`, undefined);
+                    setter.apply();
+                }
+            }
+        }
+
+        this.fix();
+    }
+
+    private fix() {
+        if (this.task.outputs.length !== 1) throw new Error("Switch must have one output!");
+        if (this.task.inputs.length % 2 !== 0) {
+            throw new Error("Switch must have an event amount of inputs!");
+        }
+
+        if (this.task.inputs.length > 2) {
+            const removeInputIndexes = new Set<number>();
+            [...Array(Math.floor(this.task.inputs.length / 2) - 1).keys()].forEach(idx => {
+                if (typeof this.task.inputs[idx*2].topic_id !== "number" && typeof this.task.inputs[idx*2 + 1].topic_id !== "number") {
+                    removeInputIndexes.add(idx*2);
+                    removeInputIndexes.add(idx*2+1);
+                }
+            });
+
+            this.task.inputs = this.task.inputs.filter((_, idx) => !removeInputIndexes.has(idx));
+        }
+
+        if (this.task.inputs.length < 2 || typeof this.task.inputs.at(-1)?.topic_id === "number" ||  typeof this.task.inputs.at(-2)?.topic_id === "number") {
+            this.task.inputs.push({ key: uuidv4() });
+            this.task.inputs.push({ key: uuidv4(), "content": "number", "type": "ts" });
+        }
+
+
+        [...Array(Math.floor(this.task.inputs.length / 2) - 1).keys()].forEach(idx => {
+            this.task.inputs[idx * 2].label = `input ${idx + 1}`;
+            this.task.inputs[idx * 2 + 1].label = `control ${idx + 1}`;
+        });
+
+        this.task.inputs.at(-2)!.label = "new input";
+        this.task.inputs.at(-1)!.label = "new control";
+
+        this.config.pairs = [...Array(Math.floor(this.task.inputs.length / 2) - 1).keys()].map(idx => ({ 
+            input: this.inputs[idx*2].topic_id,
+            control: this.inputs[idx*2 + 1].topic_id,
+        }));
+        this.config.output = this.task.outputs[0].topic_id;
+
+        const graph = this.getGraph();
+        for (const [k, v] of extractObjectPathValues(this.task.outputs[0]).entries()) {
+            if (!compareIOIgnorePaths.has(k)) {
+                graph.set(`outputs.0.${k}`, v);
+            }
+        }
+        graph.apply();
+    }
+    private getGraph() {
+        const setter = new GraphSetter(this.task);
+
+        [...Array(Math.floor(this.task.inputs.length / 2)).keys()].forEach((idx) => {
+            setter.addEdgeGenerator(`inputs.${idx*2}`, subPath => {
+                if (!subPath || compareIOIgnorePaths.has(subPath)) return [];
+                return [`outputs.0.${subPath}`];
+            });
+        });
+        setter.addEdgeGenerator("outputs.0", subPath => {
+            return [...Array(Math.floor(this.task.inputs.length / 2)).keys()].map(idx => `inputs.${idx * 2}.${subPath}`)
+        });
+
+        return setter;
+    }
+}
+
+const configurator = createCLSConfigurator((context, task) => new SwitchConfigurator(context, task));
+export default configurator;
