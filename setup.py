42e151455fe9330f4e15c85bd317584354f4deaf
leopf <l.pfob@3-klicks.de>
2024-06-20 05:25:14 +0200

    set cpp version

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
 import glob
 
-extra_args = { "extra_compile_args": ["-std=c++20"] }
+extra_args = { "extra_compile_args": ["-std=c++17"] }
 ext_modules = [ Pybind11Extension(re.sub(r"[\/\\\\]", ".", p)[:-8], sources=[p], **extra_args) for p in glob.glob("streamtasks/**/*_all.cpp", recursive=True)]
 setup(ext_modules=ext_modules)
