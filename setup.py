090cced81d0f0ee98d025e989aebfed47c8cd5f8
leopf <l.pfob@3-klicks.de>
2024-06-20 05:18:58 +0200

    remove v4l

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-import platform
 import re
-from setuptools import setup, Extension
-import pybind11
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
 import glob
 
-ext_kwargs = { "include_dirs": [ pybind11.get_include()], "language": "c++" }
-ext_modules = [ Extension(re.sub(r"[\/\\\\]", ".", p)[:-8], sources=[p], **ext_kwargs) for p in glob.glob("streamtasks/**/*_all.cpp", recursive=True)]
-
-p = platform.system().lower()
-if p == "linux":
-  ext_modules.append(Extension("streamtasks.media.v4l2", sources=[ "streamtasks/media/v4l2.cpp" ], **ext_kwargs))
-
+extra_args = { "extra_compile_args": ["-std=c++20"] }
+ext_modules = [ Pybind11Extension(re.sub(r"[\/\\\\]", ".", p)[:-8], sources=[p], **extra_args) for p in glob.glob("streamtasks/**/*_all.cpp", recursive=True)]
 setup(ext_modules=ext_modules)
