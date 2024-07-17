5e721751a2398eb45a65d4d64e6fdbb293ed6542
leopf <l.pfob@3-klicks.de>
2024-07-18 01:23:27 +0200

    named tasks

---
+++ streamtasks/system/tasks/namedinput.py
@@ -0,0 +1,37 @@
+from typing import Any
+from pydantic import BaseModel
+from streamtasks.net.messages import TopicControlData
+from streamtasks.services.protocols import AddressNames
+from streamtasks.system.named_topic_manager import NamedTopicRequestModel, NamedTopicResolvedResponseModel
+from streamtasks.system.task import Task, TaskHost
+from streamtasks.client import Client
+
+class NamedInputConfig(BaseModel):
+  name: str
+  out_topic: int
+
+class NamedInputTask(Task):
+  def __init__(self, client: Client, config: NamedInputConfig):
+    super().__init__(client)
+    self.config = config
+    self.out_topic = client.out_topic(self.config.out_topic)
+
+  async def run(self):
+    self.client.start()
+    response = await self.client.fetch(AddressNames.NAMED_TOPIC_MANAGER, "resolve_named_topic", NamedTopicRequestModel(name=self.config.name))
+    in_topic = self.client.in_topic(NamedTopicResolvedResponseModel.model_validate(response).topic)
+
+    async with self.out_topic, self.out_topic.RegisterContext(), in_topic, in_topic.RegisterContext():
+      while True:
+        data = await in_topic.recv_data_control()
+        if isinstance(data, TopicControlData): await self.out_topic.set_paused(data.paused)
+        else: await self.out_topic.send(data)
+
+class NamedInputTaskHost(TaskHost):
+  @property
+  def metadata(self): return {
+    "js:configurator": "std:namedinput",
+    "cfg:label": "Named Input"
+  }
+  async def create_task(self, config: Any, topic_space_id: int | None):
+    return NamedInputTask(await self.create_client(topic_space_id), NamedInputConfig.model_validate(config))
---
+++ streamtasks/system/tasks/namedoutput.py
@@ -0,0 +1,40 @@
+from typing import Any
+from pydantic import BaseModel
+from streamtasks.net.messages import TopicControlData
+from streamtasks.services.protocols import AddressNames
+from streamtasks.system.named_topic_manager import NamedTopicModel, NamedTopicRequestModel, NamedTopicResolvedResponseModel
+from streamtasks.system.task import MetadataDict, Task, TaskHost
+from streamtasks.client import Client
+
+class NamedOutputConfig(BaseModel):
+  name: str
+  metadata: MetadataDict
+  in_topic: int
+
+class NamedOutputTask(Task):
+  def __init__(self, client: Client, config: NamedOutputConfig):
+    super().__init__(client)
+    self.config = config
+    self.in_topic = client.in_topic(self.config.in_topic)
+
+  async def run(self):
+    self.client.start()
+
+    await self.client.fetch(AddressNames.NAMED_TOPIC_MANAGER, "put_named_topic", NamedTopicModel(name=self.config.name, metadata=self.config.metadata).model_dump())
+    response = await self.client.fetch(AddressNames.NAMED_TOPIC_MANAGER, "resolve_named_topic", NamedTopicRequestModel(name=self.config.name).model_dump())
+    out_topic = self.client.out_topic(NamedTopicResolvedResponseModel.model_validate(response).topic)
+
+    async with self.in_topic, self.in_topic.RegisterContext(), out_topic, out_topic.RegisterContext():
+      while True:
+        data = await self.in_topic.recv_data_control()
+        if isinstance(data, TopicControlData): await out_topic.set_paused(data.paused)
+        else: await out_topic.send(data)
+
+class NamedOutputTaskHost(TaskHost):
+  @property
+  def metadata(self): return {
+    "js:configurator": "std:namedoutput",
+    "cfg:label": "Named Input"
+  }
+  async def create_task(self, config: Any, topic_space_id: int | None):
+    return NamedOutputTask(await self.create_client(topic_space_id), NamedOutputConfig.model_validate(config))
