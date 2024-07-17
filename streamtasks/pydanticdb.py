3a8547108245dd9d45106ffe193c99e7988af333
leopf <l.pfob@3-klicks.de>
2024-07-18 01:06:00 +0200

    named topic manager

---
+++ streamtasks/pydanticdb.py
@@ -0,0 +1,24 @@
+import copy
+import os
+from typing import Generic, Iterable, TypeVar
+from pydantic import BaseModel, TypeAdapter
+
+T = TypeVar("T", bound=BaseModel)
+
+class PydanticDB(Generic[T]):
+  def __init__(self, model: type[T], filename: str) -> None:
+    self._list_model = TypeAdapter(list[model])
+    self._entries: list[T] = []
+    self._filename = filename
+    if os.path.exists(filename): self.load()
+
+  @property
+  def entries(self): return copy.deepcopy(self._entries)
+
+  def save(self):
+    with open(self._filename, "w") as fd: fd.write(self._list_model.dump_json(self._entries))
+
+  def load(self):
+    with open(self._filename, "r") as fd: self._entries = self._list_model.validate_json(fd.read())
+
+  def update(self, entries: Iterable[T]): self._entries = list(entries)
--- streamtasks/system/named_topic_manager.py
+++ streamtasks/system/named_topic_manager.py
@@ -1,27 +1,34 @@
 import asyncio
 import os
-from pydantic import BaseModel
+from pydantic import BaseModel, TypeAdapter
 from streamtasks.asgi import ASGIAppRunner
 from streamtasks.asgiserver import ASGIRouter, ASGIServer, HTTPContext, http_context_handler
 from streamtasks.client.discovery import register_address_name
-from streamtasks.client.fetch import FetchRequest, FetchServer
+from streamtasks.client.fetch import FetchRequest, FetchServer, new_fetch_body_not_found
 from streamtasks.env import get_data_sub_dir
 from streamtasks.net import EndpointOrAddress, Link
+from streamtasks.pydanticdb import PydanticDB
 from streamtasks.services.protocols import AddressNames
 from streamtasks.system.task import MetadataDict
 from streamtasks.system.task_web import PathRegistrationFrontend, TaskWebPathHandler
-import dbm
 
 class NamedTopicModel(BaseModel):
   name: str
   metadata: MetadataDict
 
+class NamedTopicRequestModel(BaseModel):
+  name: str
+
+class NamedTopicResolvedResponseModel(BaseModel):
+  topic: int
+
+NamedTopicListModel = TypeAdapter(list[NamedTopicModel])
+
 class NamedTopicManager(TaskWebPathHandler):
   def __init__(self, link: Link, register_endpoits: list[EndpointOrAddress] = [AddressNames.TASK_MANAGER_WEB]):
-    super().__init__(link, "/named-topics", PathRegistrationFrontend(path="std:namedtopicmanager", label="Named Topic Manager"), register_endpoits)
-    self.db = dbm.open(os.path.join(get_data_sub_dir("user-data"), "named-topics.db"), flag="c")
-
-  def __del__(self): self.db.close()
+    super().__init__(link, "/named-topics/", PathRegistrationFrontend(path="std:namedtopicmanager", label="Named Topic Manager"), register_endpoits)
+    self.db = PydanticDB(NamedTopicModel, os.path.join(get_data_sub_dir("user-data"), "named-topics.json"))
+    self.topic_map: dict[str, int] = {}
 
   async def run_inner(self):
     await register_address_name(self.client, AddressNames.NAMED_TOPIC_MANAGER)
@@ -31,13 +38,37 @@ class NamedTopicManager(TaskWebPathHandler):
     server = FetchServer(self.client)
 
     @server.route("list_named_topics")
-    async def _(req: FetchRequest): pass
+    async def _(req: FetchRequest): await req.respond(NamedTopicListModel.dump_python(self.db.entries))
 
     @server.route("get_named_topic")
-    async def _(req: FetchRequest): pass
+    async def _(req: FetchRequest):
+      try:
+        data = NamedTopicRequestModel.model_validate(req.body)
+        await req.respond(next(e for e in self.db.entries if e.name == data.name).model_dump())
+      except KeyError: await req.respond_error(new_fetch_body_not_found("named topic not found!"))
+
+    @server.route("put_named_topic")
+    async def _(req: FetchRequest):
+      try:
+        data = NamedTopicModel.model_validate(req.body)
+        self.db.update([e for e in self.db.entries if e.name != data.name] + [data])
+        self.db.save()
+        await req.respond(data.model_dump())
+      except KeyError: await req.respond_error(new_fetch_body_not_found("named topic not found!"))
+
+    @server.route("delete_named_topic")
+    async def _(req: FetchRequest):
+      data = NamedTopicRequestModel.model_validate(req.body)
+      self.db.update(e for e in self.db.entries if e.name != data.name)
+      self.db.save()
+      await req.respond(None)
 
     @server.route("resolve_named_topic")
-    async def _(req: FetchRequest): pass
+    async def _(req: FetchRequest):
+      data = NamedTopicRequestModel.model_validate(req.body)
+      if next((1 for e in self.db.entries if e.name == data.name), None) is None: self.db.update(self.db.entries + [ NamedTopicModel(name=data.name, metadata={}) ])
+      if data.name not in self.topic_map: self.topic_map[data.name] = next(await self.client.request_topic_ids(1))
+      await req.respond(NamedTopicResolvedResponseModel(topic=self.topic_map[data.name]).model_dump())
 
     await server.run()
 
@@ -53,17 +84,29 @@ class NamedTopicManager(TaskWebPathHandler):
 
     @router.get("/api/named-topics")
     @http_context_handler
-    async def _(ctx: HTTPContext): await ctx.respond_json([  ])
+    async def _(ctx: HTTPContext): await ctx.respond_json_raw(NamedTopicListModel.dump_json(self.db.entries))
+
+    @router.put("/api/named-topic")
+    @http_context_handler
+    async def _(ctx: HTTPContext):
+      data = NamedTopicModel.model_validate_json(await ctx.receive_json_raw())
+      self.db.update([e for e in self.db.entries if e.name != data.name] + [data])
+      self.db.save()
+      await ctx.respond_json(data.model_dump())
+
+    @router.get("/api/named-topics/{name}")
+    @http_context_handler
+    async def _(ctx: HTTPContext):
+      try: await ctx.respond_json(next(e for e in self.db.entries if e.name == ctx.params["name"]).model_dump())
+      except KeyError: await ctx.respond_status(404)
 
     @router.delete("/api/named-topics/{name}")
     @http_context_handler
     async def _(ctx: HTTPContext):
-      await ctx.respond_status(200)
-
-    @router.post("/api/named-topic")
-    @http_context_handler
-    async def _(ctx: HTTPContext):
-      req = NamedTopicModel.model_validate_json(await ctx.receive_json_raw())
-      await ctx.respond_json(req.model_dump())
+      try:
+        self.db.update(e for e in self.db.entries if e.name != ctx.params["name"])
+        self.db.save()
+        await ctx.respond_status(200)
+      except KeyError: await ctx.respond_status(404)
 
     await ASGIAppRunner(self.client, app).run()
