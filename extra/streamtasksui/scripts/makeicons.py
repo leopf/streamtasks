4970662e41dcced4a257f57621d040ef95b694a0
leopf <l.pfob@3-klicks.de>
2024-07-27 20:13:43 +0200

    fix windows icon

--- extra/streamtasksui/scripts/makeicons.py
+++ extra/streamtasksui/scripts/makeicons.py
@@ -4,6 +4,7 @@ icon_base = "src/streamtasksui/resources/icon"
 
 raw_icon = Image.open("src/streamtasksui/resources/icon.png")
 raw_icon.resize((64, 64)).save(icon_base + ".icns")
+raw_icon.save(icon_base + ".ico")
 
 png_sizes = [16, 32, 64, 128, 256, 512]
 for size in png_sizes: raw_icon.resize((size, size)).save(icon_base + f"-{size}.png")
