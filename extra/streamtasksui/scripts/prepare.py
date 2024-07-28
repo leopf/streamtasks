5ecef0a84dd066c676a57526f1a9f63bf948aece
leopf <l.pfob@3-klicks.de>
2024-07-28 17:34:30 +0200

    build fixes

--- extra/streamtasksui/scripts/prepare.py
+++ extra/streamtasksui/scripts/prepare.py
@@ -1,5 +1,7 @@
 from PIL import Image
+import toml
 
+# make icons
 icon_base = "src/streamtasksui/resources/icon"
 
 raw_icon = Image.open("src/streamtasksui/resources/icon.png")
@@ -8,3 +10,9 @@ raw_icon.save(icon_base + ".ico")
 
 png_sizes = [16, 32, 64, 128, 256, 512]
 for size in png_sizes: raw_icon.resize((size, size)).save(icon_base + f"-{size}.png")
+
+# fix version
+ui_pp = toml.load("pyproject-template.toml")
+core_pp = toml.load("../../pyproject.toml")
+ui_pp["tool"]["briefcase"]["version"] = core_pp["project"]["version"]
+toml.dump(ui_pp, open("pyproject.toml", "w"))
