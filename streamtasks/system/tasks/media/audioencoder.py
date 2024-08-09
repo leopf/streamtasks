838d10d5d59838a07953723b2354a570d5ca8e17
leopf <l.pfob@3-klicks.de>
2024-08-09 08:25:32 +0200

    sync audio encoder

--- streamtasks/system/tasks/media/audioencoder.py
+++ streamtasks/system/tasks/media/audioencoder.py
@@ -1,17 +1,17 @@
+from contextlib import asynccontextmanager
 from fractions import Fraction
+import queue
 from typing import Any
 from pydantic import BaseModel, ValidationError
-from streamtasks.debugging import ddebug_value
-from streamtasks.env import DEBUG_MEDIA
 from streamtasks.media.audio import AudioCodecInfo, AudioFrame
 from streamtasks.net.serialization import RawData
 from streamtasks.message.types import TimestampChuckMessage
 from streamtasks.media.packet import MediaMessage
 from streamtasks.system.tasks.media.utils import MediaEditorFields
 from streamtasks.system.configurators import EditorFields, IOTypes, static_configurator
-from streamtasks.system.task import Task, TaskHost
+from streamtasks.system.task import SyncTask, TaskHost
 from streamtasks.client import Client
-from streamtasks.utils import hertz_to_fintervall
+from streamtasks.utils import context_task, hertz_to_fintervall
 
 class AudioEncoderConfigBase(BaseModel):
   in_sample_format: IOTypes.SampleFormat = "s16"
@@ -26,7 +26,7 @@ class AudioEncoderConfig(AudioEncoderConfigBase):
   out_topic: int
   in_topic: int
 
-class AudioEncoderTask(Task):
+class AudioEncoderTask(SyncTask):
   def __init__(self, client: Client, config: AudioEncoderConfig):
     super().__init__(client)
     self.out_topic = self.client.out_topic(config.out_topic)
@@ -36,29 +36,40 @@ class AudioEncoderTask(Task):
     self.time_base = hertz_to_fintervall(config.rate)
     self.t0: int | None = None
 
-    out_codec_info = AudioCodecInfo(codec=config.encoder, sample_rate=config.rate, sample_format=config.out_sample_format, channels=config.channels, options=config.codec_options)
-    self.encoder = out_codec_info.get_encoder()
+    codec_info = AudioCodecInfo(codec=config.encoder, sample_rate=config.rate, sample_format=config.out_sample_format, channels=config.channels, options=config.codec_options)
+    self.encoder = codec_info.get_encoder()
 
-  async def run(self):
+    self.frame_data_queue: queue.Queue[TimestampChuckMessage] = queue.Queue()
+
+  @asynccontextmanager
+  async def init(self):
     try:
-      async with self.out_topic, self.out_topic.RegisterContext(), self.in_topic, self.in_topic.RegisterContext():
+      async with self.out_topic, self.out_topic.RegisterContext(), self.in_topic, self.in_topic.RegisterContext(), context_task(self._run_receiver()):
         self.client.start()
-        while True:
-          try:
-            data = await self.in_topic.recv_data()
-            message = TimestampChuckMessage.model_validate(data.data)
-            if self.t0 is None: self.t0 = message.timestamp
-
-            frame = AudioFrame.from_buffer(message.data, self.config.out_sample_format, self.config.channels, self.config.rate)
-            frame.set_ts(Fraction(message.timestamp - self.t0, 1000), self.time_base)
-
-            packets = await self.encoder.encode(frame)
-            for packet in packets:
-              if DEBUG_MEDIA(): ddebug_value("audio encoder time", float(packet.dts * self.time_base))
-              await self.out_topic.send(RawData(MediaMessage(timestamp=int(self.t0 + packet.dts * self.time_base * 1000), packet=packet).model_dump()))
-          except ValidationError: pass
+        yield
     finally:
       self.encoder.close()
+
+  async def _run_receiver(self):
+    while True:
+      try:
+        data = await self.in_topic.recv_data()
+        self.frame_data_queue.put(TimestampChuckMessage.model_validate(data.data))
+      except ValidationError: pass
+
+  def run_sync(self):
+    timeout = 0.5
+    while not self.stop_event.is_set():
+      try:
+        message = self.frame_data_queue.get(timeout=timeout)
+        if self.t0 is None: self.t0 = message.timestamp
+        frame = AudioFrame.from_buffer(message.data, self.config.out_sample_format, self.config.channels, self.config.rate)
+        frame.set_ts(Fraction(message.timestamp - self.t0, 1000), self.time_base)
+        packets = self.encoder.encode_sync(frame)
+        for packet in packets:
+          self.send_data(self.out_topic, RawData(MediaMessage(timestamp=int(self.t0 + packet.dts * self.time_base * 1000), packet=packet).model_dump()))
+        self.frame_data_queue.task_done()
+      except queue.Empty: pass
 
 class AudioEncoderTaskHost(TaskHost):
   @property
