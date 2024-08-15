026dee79d9a7026ca59fb83f0ddf026f100dc9cf
leopf <l.pfob@3-klicks.de>
2024-08-15 13:02:43 +0200

    fix broker

--- examples/broker.py
+++ examples/broker.py
@@ -11,10 +11,8 @@ async def main():
   parser.add_argument("--log-file", help="Log file.", default=None, type=lambda a: None if a is None else Path(a))
 
   args = parser.parse_args()
-  print(args)
   logging.basicConfig(level=logging._nameToLevel[args.log_level.upper()], filename=args.log_file)
 
-  server = create_server(args.url)
-  await server.run()
+  await create_server(args.url).run()
 
 if __name__ == "__main__": asyncio.run(main())
